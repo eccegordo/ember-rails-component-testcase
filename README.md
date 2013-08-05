61f0624c622d353a3f7ff6bfe1e1bebed3e58d10
Gordon Potter <gordon.potter@isilon.com>
2013-08-05 15:00:05 -0700

    first commit, most basic foo component created, does not display in handlebars application template

---
+++ README.md
@@ -0,0 +1,13 @@
+A minimalist rails app using ember rails.
+
+This is to demonstrate the use of components per the Ember JS guide
+
+http://emberjs.com/guides/components/defining-a-component/
+
+This app does nothing in particular but attempt to define handlebars component at
+
+app/assets/javscripts/component/foo.hbs
+
+and display the component in the application handlebars template at
+
+app/assets/javscripts/templates/application.hbs
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+App::Application.config.secret_token = 'bc931993217ce397da49360abf8b01f630ca33e8393980cbb729d54728b8acdce61f0998c68b700f345c04694c176c1fb1d5cc9cb5951eee45869cb4f5c13733'
